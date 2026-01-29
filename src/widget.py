c8d8af8302a9e5acc07d453fa9293735a6c712de
evgeniy_shustrov <evgen1246@yandex.ru>
2026-01-29 09:28:49 +0300

    update module add function

--- src/widget.py
+++ src/widget.py
@@ -2,6 +2,7 @@ import src.masks
 
 
 def mask_account_card(account_and_num: str | int) -> str:
+    """Функция, которая маскирует номера счетов и карт"""
     account_card_split = account_and_num.split()
 
     for i, word in enumerate(account_card_split):
@@ -14,4 +15,13 @@ def mask_account_card(account_and_num: str | int) -> str:
         account_card = ' '.join(account_card_split)
     return account_card
 
+def get_date(date_full: str)->str:
+    """Функция возврщает значение даты по формату "ДД.ММ.ГГГГ" """
+
+    year = date_full[:4]
+    month = date_full[5:7]
+    day = date_full[8:10]
+    date_only = f'{day}.{month}.{year}'
+
+    return date_only
 
