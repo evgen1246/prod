2fe082f1b346c01b9fcbff5fecee5592a2e41c4a
evgeniy_shustrov <evgen1246@yandex.ru>
2026-02-11 09:37:55 +0300

    update processing func sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,3 @@
-from datetime import datetime
-
-
 def filter_by_state(data: list, state: str = 'EXECUTED') -> list:
     """Функция возвращает новый список словарей, содержащий только те словари, у которых ключ state."""
 
@@ -8,6 +5,9 @@ def filter_by_state(data: list, state: str = 'EXECUTED') -> list:
     return filtered_data
 
 
-def sort_by_date(data: list[], reverse_order: bool = True) -> list:
-    """Сортирует список словарей по дате в указанном порядке(убывание)"""
-    return sorted(data, key=lambda x: datetime.strptime(x["date"], "%Y-%m-%dT%H:%M:%S.%f"), reverse=reverse_order)
+def sort_by_date(data: list, reverse: bool = True) -> list:
+    """
+    Сортирует список словарей по дате в порядке убывания (по умолчанию).
+    """
+
+    return sorted(data, key=lambda x: x['date'], reverse=reverse)
