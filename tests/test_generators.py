cd54a90403625d57638daecc602fbdfe827de138
evgeniy_shustrov <evgen1246@yandex.ru>
2026-02-24 21:38:36 +0300

    Модификация  test_generators и доб. html

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,8 +1,10 @@
+import pytest
+
 from src.generators import card_number_generator, filter_by_currency, transaction_descriptions
+from tests.conftest import num_card
 
 
 def test_filter_by_currency(test_filter):
-
     usd_transactions = list(filter_by_currency(test_filter, "USD"))
     rub_transactions = list(filter_by_currency(test_filter, "RUB"))
     assert len(usd_transactions) == 1
@@ -22,6 +24,18 @@ def test_transaction_descriptions(test_filter, description):
     assert descriptions == description
 
 
+# @pytest.mark.parametrize("start","stop" num_card)
 def test_card_number_generator(num_card):
     assert list(card_number_generator(1, 5)) == num_card
     assert len(list(card_number_generator(1, 5))) == 5
+
+
+@pytest.mark.parametrize("start, stop, expected", [(1, 5,
+                                                  ["0000 0000 0000 0001",
+                                                   "0000 0000 0000 0002",
+                                                   "0000 0000 0000 0003",
+                                                   "0000 0000 0000 0004",
+                                                   "0000 0000 0000 0005"])])
+def test_card_generator(start, stop, expected):
+    gen_num = list(card_number_generator(start, stop))
+    assert  gen_num == expected
